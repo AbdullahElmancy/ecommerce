6242ef4032f5b901ae482f17c9218ebf5b5d553d
AbdullahElmancy <aelmancee@gmail.com>
2025-08-22 20:50:11 +0300

    some update in error handle

--- src/Middlewares/error.middleware.ts
+++ src/Middlewares/error.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { errorResponse } from "../utils/response";
 
-export const errorHandler = (err: { status: number; message: string,errors?:any }, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     return errorResponse(res,err.message || "Internal server error",err.status || 500, err.errors)
 };
 
